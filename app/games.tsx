a9f10114cc879c9e9c614f03a0cfbe2575e56719
Helio <helioroberto115@gmail.com>
2026-09-18 10:18:06 -0300

    Atualizações

--- app/games.tsx
+++ app/games.tsx
@@ -128,23 +128,23 @@ export const games: Game[] = [
     'Habilidade manual',
   ],
 },
-{
-  name: 'Bolinha de gude',
-  icon: '●',
-  image: '/imagens/bolinha-de-gude.png',
-  color: 'orange',
-  category: ['Coordenação'],
-  desc: 'Uma brincadeira clássica que testa a pontaria e a precisão dos jogadores.',
-  how: 'Desenhe um círculo no chão e coloque algumas bolinhas de gude dentro. Cada jogador deve tentar acertar as bolinhas com a sua própria bolinha, usando os dedos. O objetivo é tirar as bolinhas do círculo.',
-  materials: 'Bolinhas de gude e giz',
-  players: '2 ou mais',
-  age: 'A partir de 5 anos',
-  benefits: [
-    'Coordenação motora',
-    'Concentração',
-    'Precisão',
-    'Raciocínio',
-   ],
- },
+  {
+    name: 'Bolinha de gude',
+    icon: '⚫',
+    image: '/imagens/bolinha-de-gude.png',
+    color: 'orange',
+    category: ['Coordenação', 'Em Grupo'],
+    desc: 'Uma brincadeira clássica que testa a pontaria e a precisão dos jogadores.',
+    how: 'Desenhe um círculo no chão e coloque algumas bolinhas de gude dentro. Cada jogador deve tentar acertar as bolinhas com a sua própria bolinha, usando os dedos.',
+    materials: 'Bolinhas de gude e giz',
+    players: '2 ou mais',
+    age: 'A partir de 5 anos',
+    benefits: [
+      'Coordenação motora',
+      'Concentração',
+      'Precisão',
+      'Raciocínio'
+    ]
+  }
 ];
 
